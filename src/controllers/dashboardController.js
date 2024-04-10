b81f7b4e130524b8997f3bf72b263b0c706f62b1
PandiSaL <166236338+PanPanZeL@users.noreply.github.com>
2024-04-10 23:48:18 +0800

    Added Update Userlist

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,12 +1,12 @@
-const UserList = require('../models/registrationModels');
+const UserList = require("../models/registrationModels");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // View all Users from UserLists (@Keith)
 const viewAllUsers = async (req, res) => {
   try {
-    const viewallUsers = await UserList.find({}).sort({createdAt: -1})
-    res.status(200).json(viewallUsers)
+    const viewallUsers = await UserList.find({}).sort({ createdAt: -1 });
+    res.status(200).json(viewallUsers);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -14,29 +14,44 @@ const viewAllUsers = async (req, res) => {
 
 // GET one user by objectID SAMPLE
 const viewUser = async (req, res) => {
-  const { id } = req.params
-    try{
-      if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({error: 'No such user (wrong objectid'})
-      }
-
-      const user = await UserList.findById(id)
-
-      if (!user) {
-        return res.status(404).json({error: 'No such user'})
-      }
-
-      res.status(200).json(user)
-    }catch (error) {
-      res.status(500).json({ message: error.message });
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "No such user (wrong objectid" });
     }
-  }
 
+    const user = await UserList.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ error: "No such user" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // Update a user by objectID @ Neil
 const updateUser = async (req, res) => {
   try {
-    // insert codes here
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "unavailable ObjectId" });
+    }
+
+    const user = await UserList.findOneAndUpdate(
+      { _id: id },
+      {
+        ...req.body,
+      },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(400).json({ error: "No such user" });
+    }
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -55,5 +70,5 @@ module.exports = {
   viewUser,
   deleteUser,
   updateUser,
-  viewAllUsers
-}
+  viewAllUsers,
+};

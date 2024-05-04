8cfc5ebbe0a8243665168a7811a6aa478f929186
PZ991 <pureanimationz@gmail.com>
2024-05-05 00:59:56 +0800

    appdev cont fixed

--- backend/controllers/applicationDevController.js
+++ backend/controllers/applicationDevController.js
@@ -14,7 +14,7 @@ class ApplicationDevtController {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    const duplicate = await Members.findOne({ teamName }).lean().exec();
+    const duplicate = await Appdev.findOne({ teamName }).lean().exec();
 
     if (duplicate) {
       return res.status(409).json({ message: "Team already registered." });

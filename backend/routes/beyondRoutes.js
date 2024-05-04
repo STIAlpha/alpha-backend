d1dcb25d7d5996004248ae1d946b2e334d94eec3
PZ991 <pureanimationz@gmail.com>
2024-05-05 02:49:09 +0800

    beyond cont fix

--- backend/routes/beyondRoutes.js
+++ backend/routes/beyondRoutes.js
@@ -1,7 +1,7 @@
 // beyondRoutes.js
 const express = require('express');
 const router = express.Router();
-const BeyondController = require('./beyondController');
+const BeyondController = require('../controllers/beyondController');
 
 router.post('/', BeyondController.registerToBeyondEvent);
 router.get('/:teamName', BeyondController.getSingleBeyondTeam);

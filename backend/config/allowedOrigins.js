2392a1def24b6557489d791702937672babdbd0e
Lev Skyler Roan <skylevroan@gmail.com>
2024-05-18 21:51:10 +0800

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'https://alpha-backend-api.netlify.app',
+    'http://localhost:3000',
     'http://localhost:3500',
     'https://www.dandrepairshop.com',
     'https://dandrepairshop.com'

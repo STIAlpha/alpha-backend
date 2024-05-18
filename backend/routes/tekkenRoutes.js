a4b461e153293cd40b2938325aa36658094c629d
Skygild <skylevroan@gmail.com>
2024-05-18 19:15:10 +0800

    changed file name

--- backend/routes/tekkenRoutes.js
+++ backend/routes/tekkenRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const TekkenController = require('../controllers/tekkenController')
+const TekkenController = require('../controllers/TekkenController')
 
 router.route('/').get(TekkenController.getTekkenEntries);
  router.route('/').post(TekkenController.registerToTekkenEvent);

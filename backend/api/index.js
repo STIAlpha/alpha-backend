c7372bf4af5ee71545e131cf1dfff8b565fbfc69
Lev Skyler Roan <skylevroan@gmail.com>
2024-05-19 10:01:50 +0800

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,101 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const { logger, logEvents } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3500
+
+console.log(process.env.NODE_ENV)
+
+connectDB()
+
+app.use(logger)
+
+// app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.get('/auth', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'auth.html'));
+  });
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+})
+app.get('/get-upload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'sample.html'));
+  });
+app.use('/uploads', express.static('uploads'))
+app.use('/', require('./routes/root'))
+app.use('/users', require('./routes/userRoutes'))
+app.use('/auth', require('./routes/authRoutes'))
+app.use('/members', require('./routes/membersRoutes'))
+app.use('/officers', require('./routes/OfficersRoutes'))
+app.use('/events', require('./routes/eventRoutes'))
+app.use('/notes', require('./routes/noteRoutes'))
+app.use('/attendance', require('./routes/attendanceRoutes'))
+
+app.use('/chess', require('./routes/chessRoutes'))
+app.use('/ITQuizBee', require('./routes/ITquizbeeRoutes'))
+app.use('/CSQuizBee', require('./routes/CSquizbeeRoutes'))
+
+app.use('/appdev', require('./routes/applicationDevRoutes'))
+app.use('/webdev', require('./routes/webdevRoutes'))
+app.use('/lol', require('./routes/lolRoutes'))
+app.use('/valorant', require('./routes/valorantRoutes'))
+app.use('/tekken', require('./routes/tekkenRoutes'))
+
+app.use('/researchforum', require('./routes/researchForumRoutes'))
+app.use('/ideathon', require('./routes/ideathonRoutes'))
+app.use('/chibby', require('./routes/chibbyRoutes'))
+app.use('/beyond', require('./routes/beyondRoutes'))
+app.use('/mlbb', require('./routes/mLBBRoutes'))
+app.use('/codm', require('./routes/codmRoutes'))
+app.use('/wildrift', require('./routes/wildriftRoutes'))
+app.use('/gamejam', require('./routes/gameJamRoutes'))
+
+app.use(express.json());
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
+
 module.exports = (req, res) => {
   res.status(200).json({ message: 'Hello from Vercel!' });
 };
+
+

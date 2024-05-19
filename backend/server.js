13ef34e2c79aca7ad236d25c20931e19c9548aa1
Lev Skyler Roan <skylevroan@gmail.com>
2024-05-19 11:50:49 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,7 @@ connectDB()
 
 app.use(logger)
 
-// app.use(cors())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
+app.use(cors())
 
 app.use(express.json())
 

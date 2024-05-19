241ea71d3d5b5f51031e7a2a6ed607f0fc3caacf
NarixenoAdaxius <dauis.arionejohn@gmail.com>
2024-05-19 23:03:30 +0800

    Added alpha-sti.vercel.app

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3500',
     'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://dandrepairshop.com',
+    'http://alpha-.sti.vercel.app'
 ]
 
 module.exports = allowedOrigins
